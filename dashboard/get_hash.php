d2691f4126fb61bc0383de8584d98ed5bb239320
zaraa-kuki <azfara2006@gmail.com>
2026-03-03 20:51:21 +0700

    Delete dashboard directory

--- dashboard/get_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password_asli = "akucantik"; // PASSWORD FORM LOGIN
-$hash_aman = password_hash($password_asli, PASSWORD_DEFAULT);
-echo $hash_aman;
-?>
--- dashboard/set_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Memulai session
-
-// Menyimpan data ke dalam session
-$_SESSION['nama'] = 'admin';
-$_SESSION['role'] = 'administrator';
-
-echo "Session telah dibuat. <a href='get_session.php'>Lihat session</a>";
-?>
